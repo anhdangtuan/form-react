839c2590052fb8d0f72bb945f8473fa3ea118190
unknown <you@example.com>
2021-10-26 23:45:21 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Header from './components/header'
+import InputForm from './components/input';
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <InputForm />
+    </div>
+  );
+}
+
+export default App;
